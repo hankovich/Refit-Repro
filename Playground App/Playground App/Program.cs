838ed7ae85f309d81c24fe0d2dbf16e3465fbf39
Sviataslau Hankovich <svyatoslav.hankovich@gmail.com>
2019-04-08 10:23:02 +0300

    Add files via upload

---
+++ Playground App/Playground App/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Threading.Tasks;
+using Refit;
+
+namespace Playground_App
+{
+    public static class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var refitInterface = RestService.For<IRefitInterface>("http://www.google.com/");
+
+            await refitInterface.PingAsync();
+        }
+    }
+    public interface IRefitInterface
+    {
+        [Get("")]
+        Task PingAsync();
+    }
+}
